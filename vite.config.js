c239ed31a7ea072f726c5bb10a7031b184d4c9dc
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2025-06-30 19:20:05 +0530

    adding

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), NodePolyfillPlugin()],
+  plugins: [react(), tailwindcss(), nodePolyfills()],
 })
